8e174cd11814e30116dd6b7e6c73fb478ee7c8a5
User <user@example.com>
2025-09-10 13:55:39 +0200

    fix: add null checks for cached mongoose object

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -34,28 +34,28 @@ async function connectDB() {
     return null
   }
 
-  if (cached.conn) {
+  if (cached?.conn) {
     return cached.conn
   }
 
-  if (!cached.promise) {
+  if (!cached?.promise) {
     const opts = {
       bufferCommands: false,
     }
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose
     })
   }
 
   try {
-    cached.conn = await cached.promise
+    cached!.conn = await cached!.promise
   } catch (e) {
-    cached.promise = null
+    cached!.promise = null
     throw e
   }
 
-  return cached.conn
+  return cached!.conn
 }
 
 export default connectDB
